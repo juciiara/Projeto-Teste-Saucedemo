bdc78b6f5423db993ee5e142b7d3455004ab0cb4
juciara <juciarae.c@gmail.com>
2025-02-19 14:47:14 -0300

    Inclusão da class Executa

---
+++ src/test/java/saucedemo/automation/runner/Executa.java
@@ -0,0 +1,19 @@
+package saucedemo.automation.runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import saucedemo.automation.drivers.Drivers;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		
+		)
+
+
+public class Executa extends Drivers{
+	
+	
+
+}
